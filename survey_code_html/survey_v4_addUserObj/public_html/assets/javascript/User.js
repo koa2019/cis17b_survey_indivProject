b72f6894557fbf80d4e13334325703b621b947e4
koa2019 <no-reply>
2023-05-23 19:21:00 -0700

    Survey_v4_addUserObj *Copied User.js from Yahtzee_v5   *Replaced hiScore with votesSiz.   *Aggregated an instance of Votes.js inside of User.js constructors.   *BUG! Can’t access user.votes[i] outside of User.js.
    *Created one superObject in home.html that includes a User object and Votes object.
      *I did this because I couldn’t access user.votes[i] inside of qq_dynamic.js
    
    Signed-off-by: koa2019 <no-reply>

---
+++ survey_code_html/survey_v4_addUserObj/public_html/assets/javascript/User.js
@@ -0,0 +1,107 @@
+/* 
+ 
+*/
+// Votes Variables
+const SIZE = 3;
+
+
+// Default constructor  Called in Yahtzee.html
+function User(){   
+    //this.readNumRec();  
+    this.numRec = 0;
+    this.name="Guest";
+    this.email="email@email.com";
+    this.password="password";  
+    this.votesSiz=NUMQQ;
+    
+    console.log("Hit User Default constructor");
+}
+
+
+// Constructor #2. Called in Yahtzee.js
+function User(i){   
+    //this.readNumRec(); 
+    this.numRec = 0;
+    this.name="Guest "+i;
+    this.email="email@email.com";
+    this.password="password"; 
+    this.votesSiz=NUMQQ;
+    
+    var votes = new Array();
+    votes = new Votes;
+
+//    votes.setVoteIndx(1, 99);
+//    votes.consoleVotes();
+//    votes.consoleNumVotes();
+//    votes.increVoteIndx(1);
+//    votes.consoleVotes();
+//    votes.consoleNumVotes();
+    console.log("Hit User constructor#2");
+}
+
+//To String function of the ScoreCard Class
+User.prototype.printUser=function(){
+    console.log("numRec   = " + this.getNumRec());
+    console.log("name     = " + this.getName());
+    console.log("email    = " + this.getEmail());
+    console.log("password = " + this.getPassword());
+    console.log("votesSiz = " + this.getVotesSiz());
+    //votes.consoleVotes();
+    //votes.consoleNumVotes();
+};
+
+//*****************************************************
+//                      Mutators
+//*****************************************************
+User.prototype.setNumRec=function(num) { 
+    this.numRec = num; 
+};
+
+User.prototype.setName=function(str){
+    this.name = str;
+};
+
+User.prototype.setEmail=function(str){
+    this.email = str;
+};
+
+User.prototype.setPassword=function(str){
+    this.password = str;
+};
+
+User.prototype.setVotesSiz=function(s) {
+    this.votesSiz = s; 
+};
+
+
+//*****************************************************
+//                      Accessors
+//*****************************************************
+User.prototype.getNumRec=function(){
+    return this.numRec;
+};
+
+User.prototype.getName=function(){
+    return this.name;
+};
+
+User.prototype.getEmail=function(){
+    return this.email;
+};
+
+User.prototype.getPassword=function(){
+    return this.name;
+};
+
+User.prototype.getVotesSiz=function(){
+    return this.votesSiz;
+};
+
+
+/******************************************************************/              
+//                  REWRITE 1 RECORD TO BINARY FILE     
+/*****************************************************************/
+
+User.prototype.reWrtRecord=function(){
+    console.log("Hit reWrtRecord()");
+};
